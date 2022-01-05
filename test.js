ba56421624389bbf89f109dacad23ee4b1f03dd6
Fabien Bénard <fbenard.public@gmail.com>
2022-01-05 18:03:47 +0100

    Improves test

--- test.js
+++ test.js
@@ -1,19 +1,68 @@
 // External dependencies
 
+const axios = require(`axios`);
+
+
+// Internal dependencies
+
 const App = require(`./lib/App`);
+
+
+//
+
+let config =
+{
+	mao:
+	{
+		core:
+		{
+			discovery: `http://localhost:8000`,
+			service: `mao-core-test`,
+			server:
+			{
+				scheme: `http`,
+				host: `localhost`,
+				port: 8080
+			}
+		}
+	}
+}
 
 
 // Create the app
 
-let app = new App();
+let app = new App([], config);
 
 
 // Start the app
 
-app.start().catch
+app.start()
+.then
+(
+	() =>
+	{
+		axios.get(`http://localhost:8080/api/health/ready`)
+		.then
+		(
+			(response) =>
+			{
+				if (response.status === 200)
+				{
+					console.log("Application is ready!");
+				}
+				else
+				{
+					throw "BWAAAH";
+				}
+			}
+		);
+	}
+)
+.catch
 (
 	(error) =>
 	{
+		console.log(error);
 		console.log(error.message);
 	}
 );
